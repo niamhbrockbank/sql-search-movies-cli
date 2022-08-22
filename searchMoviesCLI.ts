02b00d660984c8eea6a5d20a82c6b5b7d2206bea
Niamh <niamh.brockbank@gmail.com>
2022-08-22 13:11:37 +0100

    Show favourites table for option 2

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -19,9 +19,17 @@ async function handleSearch(searchTerm : string){
 
         const results = await client.query(text, values)
         console.table(results.rows)
+        console.log('i tried to get your favourites but there are none')
 
         searchTerm = await question('Search for movie here (or q to quit): ') 
     } 
+}
+
+async function showFavourites(){
+    //TO DO: edit query so that it shows the same information that you get from searching
+        //This will probably use a JOIN
+    const results = await client.query('SELECT * FROM favourites')
+    console.table(results.rows)
 }
 
 async function execute(){  
@@ -29,20 +37,22 @@ async function execute(){
         await client.connect()
         console.log("Connected successfully")
 
+        //TO DO: format the interaction option nicer
+            //Posibbly list the options on separate lines, then ask the question like in Selene example
         let interactionOption : string = await question('Do you want to (1) Search for movies, (2) See your favourites, (3) Quit ')
-        if (interactionOption === '1'){
-            await searchForMovies()
-        }
-        else if (interactionOption === '2'){
-            console.log('favourites')
-        }
-        else if (interactionOption === '3'){
-            client.end()
-            console.log('Disconnected')
-        }
-        else {
-            interactionOption = await question('Please choose option 1 , 2 , or 3 ')
-        }
+        while (interactionOption !== '3') {
+            if (interactionOption === '1'){
+                await searchForMovies()
+                interactionOption = await question('Do you want to (1) Search for movies, (2) See your favourites, (3) Quit ')
+            }
+            else if (interactionOption === '2'){
+                await showFavourites()
+                interactionOption = await question('Do you want to (1) Search for movies, (2) See your favourites, (3) Quit ')
+            }
+            else {
+                interactionOption = await question('Please choose option (1) , (2) , or (3) ')
+            }
+        } 
     } 
     catch (ex) {
         console.log(`Error connecting: ${ex}`)
